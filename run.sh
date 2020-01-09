12aeb55da47837514cb339ab7aea5b452b5a5b7d
cloughurd <benkclough@gmail.com>
2020-01-09 12:23:31 -0700

    Added imports to run file

--- run.sh
+++ run.sh
@@ -5,8 +5,17 @@ sudo apt install software-properties-common
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt install python3
 sudo apt install python3-pip
+
+## Sound processing packages
 sudo apt-get install libsndfile1
 pip3 install pysoundfile
 pip3 install numpy
+
+## Auth packages
+pip3 install jwcrypto
+pip3 install pyjwt
+
+## DB packages
+pip3 install mysql-connector-python
 
 python3 app.py
