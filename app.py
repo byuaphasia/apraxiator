4fc9b1c9029b044f34e28a36d0e55c04440fda3e
Ben Clough <benkclough@gmail.com>
2020-01-09 20:11:11 -0700

    Recording access via mysql db works

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify, send_file
 import os
+import io
 
 from wsdcalculator.calculatewsd import WSDCalculator
 from wsdcalculator.processenvironment import get_environment_percentile
-# from wsdcalculator.storage.sqlstorage import SQLStorage
-from wsdcalculator.storage.memorystorage import MemoryStorage
+from wsdcalculator.storage.sqlstorage import SQLStorage
+# from wsdcalculator.storage.memorystorage import MemoryStorage
 
 import logging
 from log.setup import setup_logger
@@ -13,21 +14,22 @@ setup_logger()
 logger = logging.getLogger(__name__)
 app = Flask(__name__)
 
-# storage = SQLStorage()
-storage = MemoryStorage()
+storage = SQLStorage()
+# storage = MemoryStorage()
 calculator = WSDCalculator(storage)
 
 @app.route('/evaluation', methods=['POST'])
 def create_evaluation():
     logger.info('[event=create-evaluation][remoteAddress=%s]', request.remote_addr)
 
-    f = request.files['recording']
+    f = request.files['recording'].read()
     threshold = get_environment_percentile(f)
     id = storage.create_evaluation(threshold, '')
     result = {
         'evaluationId': id
     }
-    return result
+    logger.info('evaluation created')
+    return jsonify(result)
 
 @app.route('/evaluation/<evaluationId>', methods=['GET'])
 def get_evaluation(evaluationId):
@@ -37,13 +39,13 @@ def get_evaluation(evaluationId):
     result = {
         'attempts': [a.__dict__ for a in attempts]
     }
-    return result
+    return jsonify(result)
 
 @app.route('/evaluation/<evaluationId>/attempt', methods=['POST'])
 def process_attempt(evaluationId):
     logger.info('[event=create-attempt][evaluationId=%s][remoteAddress=%s]', evaluationId, request.remote_addr)
 
-    f = request.files['recording']
+    f = request.files['recording'].read()
     syllable_count = request.args.get('syllableCount')
     syllable_count = int(syllable_count)
     term = request.args.get('word')
@@ -61,24 +63,26 @@ def process_attempt(evaluationId):
         'attemptId': id,
         'wsd': wsd
     }
-    return result
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    return jsonify(result)
 
 @app.route('/evaluation/<evaluationId>/attempt/<attemptId>/recording', methods=['POST', 'GET'])
 def save_recording(evaluationId, attemptId):
     if request.method == 'POST':
         logger.info('[event=save-recording][evaluationId=%s][attemptId=%s][remoteAddress=%s]', evaluationId, attemptId, request.remote_addr)
 
-        f = request.files['recording']
+        f = request.files['recording'].read()
+        print(type(f))
         id = storage.save_recording(f, evaluationId, attemptId, '')
         result = {
             'attemptId': id
         }
+        result = jsonify(result)
+        return result
     else:
         logger.info('[event=get-recording][evaluationId=%s][attemptId=%s][remoteAddress=%s]', evaluationId, attemptId, request.remote_addr)
 
         f = storage.get_recording(evaluationId, attemptId, '')
-        result = f
-    return result
+        return send_file(io.BytesIO(f), mimetype='audio/wav')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=8080)
--- wsdcalculator/storage/sqlstorage.py
+++ wsdcalculator/storage/sqlstorage.py
@@ -1,5 +1,7 @@
-import mysql.connector
+import pymysql
 import os
+import logging
+import pickle
 
 from .evaluationstorage import EvaluationStorage
 from .recordingstorage import FileStorage
@@ -8,10 +10,13 @@ from ..models.attempt import Attempt
 class SQLStorage(EvaluationStorage, FileStorage):
     def __init__(self):
         p = os.environ.get('MYSQL_PASSWORD', None)
-        self.db = mysql.connector.connect(host='localhost', user='root', password=p, database='apraxiator')
+        self.db = pymysql.connections.Connection(user='root', password=p, database='apraxiator')
+        self.logger = logging.getLogger(__name__)
+        self._create_tables()
 
     def _add_evaluation(self, e):
-        sql = 'INSERT INTO evaluations (evaluation_id, owner_id, ambiance_threshold) VALUES (%s, %s, %s)'
+        sql = 'INSERT INTO evaluations (evaluation_id, owner_id, ambience_threshold) VALUES (%s, %s, %s)'
+        self.logger.info(e.id)
         val = (e.id, e.owner_id, e.ambiance_threshold)
         self._execute_insert_query(sql, val)
 
@@ -22,7 +27,7 @@ class SQLStorage(EvaluationStorage, FileStorage):
         return res[2]
     
     def _add_attempt(self, a):
-        sql = 'INSERT INTO attempts (attempt_id, evaluation_id, term, wsd, duration) VALUE (%s, %s, %s, %s, %s)'
+        sql = 'INSERT INTO attempts (attempt_id, evaluation_id, word, wsd, duration) VALUE (%s, %s, %s, %s, %s)'
         val = (a.id, a.evaluation_id, a.term, a.wsd, a.duration)
         self._execute_insert_query(sql, val)
 
@@ -57,9 +62,46 @@ class SQLStorage(EvaluationStorage, FileStorage):
         sql = 'INSERT INTO recordings (attempt_id, recording) VALUE (%s, %s)'
         val = (attempt_id, recording)
         self._execute_insert_query(sql, val)
+        return attempt_id
 
     def _get_recording(self, attempt_id):
         sql = 'SELECT recording FROM recordings WHERE attempt_id = %s'
         val = (attempt_id,)
         res = self._execute_select_query(sql, val)
         return res[0]
+
+    def _create_tables(self):
+        create_evaluations_statement = ("CREATE TABLE IF NOT EXISTS `evaluations` ("
+            "`evaluation_id` varchar(36) NOT NULL,"
+            "`owner_id` varchar(36) NOT NULL,"
+            "`ambiance_threshold` float DEFAULT NULL,"
+            "`date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,"
+            "PRIMARY KEY (`evaluation_id`)"
+            ");"
+        )
+        create_attempts_statement = ("CREATE TABLE IF NOT EXISTS `attempts` ("
+            "`evaluation_id` varchar(48) NOT NULL,"
+            "`word` varchar(48) NOT NULL,"
+            "`attempt_id` varchar(48) NOT NULL,"
+            "`wsd` float NOT NULL,"
+            "`duration` float NOT NULL,"
+            "`date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,"
+            "PRIMARY KEY (`attempt_id`),"
+            "KEY `evaluation_id_idx` (`evaluation_id`),"
+            "CONSTRAINT `evaluation_id` FOREIGN KEY (`evaluation_id`) REFERENCES `evaluations` (`evaluation_id`)"
+            ");"
+        )
+        create_recordings_statement = ("CREATE TABLE IF NOT EXISTS `recordings` ("
+            "`recording_id` int AUTO_INCREMENT NOT NULL,"
+            "`attempt_id` varchar(48) NOT NULL,"
+            "`date_created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,"
+            "`recording` mediumblob NOT NULL,"
+            "PRIMARY KEY (`recording_id`),"
+            "KEY `attempt_id_idx` (`attempt_id`),"
+            "CONSTRAINT `attempt_id` FOREIGN KEY (`attempt_id`) REFERENCES `attempts` (`attempt_id`)"
+            ");"
+        )
+        c = self.db.cursor()
+        c.execute(create_evaluations_statement)
+        c.execute(create_attempts_statement)
+        c.execute(create_recordings_statement)
