1de453023a86858d7d0d6767641a24401c63f57a
cloughurd <benkclough@gmail.com>
2020-01-30 12:33:22 -0700

    Re-ran benchmarks and updated threshold percentile to best

--- wsdcalculator/processenvironment.py
+++ wsdcalculator/processenvironment.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from .wavreader.wavreader import read
 
-def get_environment_percentile(env_recording, percentile=99.9):
+def get_environment_percentile(env_recording, percentile=99.5):
     audio, _ = read(env_recording)
     audio = np.abs(audio)
     return float(np.percentile(audio, percentile))
