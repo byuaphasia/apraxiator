edb6739391bbea1d79e47a3861aba6374aec2502
cloughurd <benkclough@gmail.com>
2020-03-05 11:21:17 -0700

    Separated out pip requirements

--- log/setup.py
+++ log/setup.py
@@ -1,8 +1,10 @@
 import logging
 from datetime import date
+import warnings
 
 
 def setup_logger():
+    warnings.filterwarnings('ignore')
     log_file = 'log/app.{}.log'.format(date.today().isoformat())
     log_level = logging.INFO
     log_fmt = '[%(asctime)s] %(levelname)s (%(module)s-%(funcName)s-%(lineno)d) : %(message)s'
