ec6fcf828da94887a5a40e7e0130fc323c1b0d46
cloughurd <benkclough@gmail.com>
2020-04-07 19:18:19 -0600

    Fix tests

--- tests/testutils/exportutils.py
+++ tests/testutils/exportutils.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+
+
 def gen_export_data(num_rows=3):
     data = []
     for i in range(num_rows):
@@ -7,7 +10,9 @@ def gen_export_data(num_rows=3):
             f'AT-{i}',
             3.14,
             3.14,
-            '2000-01-01',
+            True,
+            3,
+            datetime.now(),
             '50',
             'male',
             'normal'
